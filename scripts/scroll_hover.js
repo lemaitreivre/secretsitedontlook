a8b4832d947b81ecd901a323471a8b636e6ce10d
maitreivre <facelessperformer@yandex.ru>
2024-05-23 17:54:53 +0500

    резиновая

--- scripts/scroll_hover.js
+++ scripts/scroll_hover.js
@@ -10,7 +10,7 @@ function onEntry(entry) {
   let options = {
     threshold: [0.5] };
   let observer = new IntersectionObserver(onEntry, options);
-  let elements = document.querySelectorAll('.serv__title,.serv__text,.serv__img,.about__title,.about__text,.about__item,.welcome,.big__logo,.menu,.form__title,.form,.form__head,.portfolio__inner-title,.portfolio__inner-text,.left-img-port,.right-img-port,.triple__photo-img,.portfolio__content-all-text,.single-img,.portfolio__content-all-text-left,.portfolio__content-all-text-right,.portfolio__right-images,.portfolio__left-images,.send,.input,.contacts__head,.send__info');
+  let elements = document.querySelectorAll('.serv__title,.serv__text,.serv__img,.about__title,.about__text,.about__item,.welcome,.big__logo,.menu,.form__title,.form,.form__head,.portfolio__inner-title,.portfolio__inner-text,.left-img-port,.right-img-port,.triple__photo-img,.portfolio__content-all-text,.single-img,.portfolio__content-all-text-left,.portfolio__content-all-text-right,.portfolio__right-images,.portfolio__left-images,.send,.input,.contacts__head,.send__info,.first_send');
   
   for (let elm of elements) {
     observer.observe(elm);
